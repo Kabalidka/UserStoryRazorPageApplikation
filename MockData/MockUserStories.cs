9ce22e0d7d1696aa95c3d493c1a41cc67c73816f
lidka <lidka@PC-LIDKA>
2022-11-25 13:16:10 +0100

    Add project files.

---
+++ MockData/MockUserStories.cs
@@ -0,0 +1,23 @@
+﻿using System.Runtime.ConstrainedExecution;
+using UserStoryRazorPageApplikation.Model;
+
+namespace UserStoryRazorPageApplikation.MockData
+{
+    public class MockUserStories
+    {
+        private static List<UserStory> userStories = new List<UserStory>()
+{
+
+    new UserStory("Create Story", "As P.O I want to create a new Story So ..."),
+    new UserStory("Edit Story", "As P.O I want to edit a Story So ..."),
+    new UserStory("Move Story", "As team member I want to move a Story So ..."),
+    new UserStory("Delete Story", "As team member I want to delete a Story So ...")
+       
+        };
+
+        public List<UserStory> GetMockUserStories() 
+        {
+            return userStories;
+        }
+    }
+}
---
+++ Model/UserStory.cs
@@ -0,0 +1,30 @@
+﻿namespace UserStoryRazorPageApplikation.Model
+{
+    public class UserStory
+
+    {
+        private string v1;
+        private string v2;
+
+        public UserStory(string v1, string v2)
+        {
+            this.v1 = v1;
+            this.v2 = v2;
+        }
+
+        public UserStory(int id, string titel, string description)
+        {
+            Id = id;
+            Titel = titel;
+            Description = description;
+        }
+        public int Id { get; set; }
+        public string Titel { get; set;}
+        public string Description { get; set; }
+
+        public override string ToString()
+        {
+            return $"{{{nameof(Id)}={Id}, {nameof(Titel)}={Titel}, {nameof(Description)}={Description}}}";
+        }
+    }
+}
